952714d2996e2d5340710d926cd10b70e667021b
Noemi <agaudi@live.com.mx>
2025-02-18 12:12:01 -0600

    first commit

---
+++ migrations/m250211_200750_migracion_yii2basic.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m250211_200750_migracion_yii2basic
+ */
+class m250211_200750_migracion_yii2basic extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m250211_200750_migracion_yii2basic cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250211_200750_migracion_yii2basic cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
