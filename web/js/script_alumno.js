1e272e3e19feee025edd3cf761fcb1a7a2685726
Noemi <105141308+Xaogai@users.noreply.github.com>
2025-03-04 13:38:17 -0600

    Revert "Back alumno"

--- web/js/script_alumno.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Simular estado del estudiante
-    let estadoEstudiante = "No has iniciado tus prácticas."; // Puedes cambiar esto según los datos
-
-    // Mostrar en el div correspondiente
-    document.getElementById("estado").textContent = estadoEstudiante;
-});
